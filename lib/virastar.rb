571d80ca8484fcc1c187b54544d21f79fdfb029e
Allen Bargi <allen.bargi@gmail.com>
2011-01-11 20:04:08 +0100

    more specs added and some of them implemented

--- lib/virastar.rb
+++ lib/virastar.rb
@@ -1,22 +1,57 @@
 require "rubygems"
 require "diffy"
 
+# TODO test in ruby 1.9
+# TODO do not replace /n
 require 'jcode'
 $KCODE = 'u'
 
 module Virastar
-  
+
   # TODO: make a class and then extend string with another module that initialize that class
-  
+
   def persian_cleanup
-    bad  = "1234567890,;كي٠١٢٣٤٥٦٧٨٩"
-    good = "۱۲۳۴۵۶۷۸۹۰،؛کی۰۱۲۳۴۵۶۷۸۹"
+    # replace double dash to ndash and triple dash to mdash
+    self.gsub!(/-{3}/,'—')
+    self.gsub!(/-{2}/,'–')
+
+    # replace three dots with ellipsis
+    self.gsub!(/\s*\.{3,}/,'…')
+
+    # replace English qoutes with their Persian equivalent
+    self.gsub!(/(["'`]+)(.+)(\1)/, '«\2»')
+
+    # should convert ه ی to ه
+    self.gsub!(/(\S)(ه[\s‌]+ی)(\s)/, '\1هٔ\3')
+
+    # should fix spacing for () [] {}  “” «» `` '' ""
+    self.gsub!(/\s*(\()\s*([^}]+)\s*(\))\s*/,' \1\2\3 ')
 
     # character replacement
+    bad  = "1234567890,;كي٠١٢٣٤٥٦٧٨٩"
+    good = "۱۲۳۴۵۶۷۸۹۰،؛کی۰۱۲۳۴۵۶۷۸۹"
     self.tr!(bad,good)
 
-    # semicolon and colon should have one space after and no space before
-    self.gsub!(/\s*([:؛،.؟!])\s*/, '\1 ')
+
+    # == Aggressive Editing ===========================================
+
+    # replace more than one ! or ? mark with just one
+    self.gsub!(/(!){2,}/, '\1')
+    self.gsub!(/(؟){2,}/, '\1')
+
+    # should remove all kashida
+    self.gsub!(/ـ+/,"")
+
+    # =================================================================
+
+    # : ; , . ! ? and their persian equivalents should have one space after and no space before
+    self.gsub!(/\s*([:;,؛،.؟!]{1})\s*/, '\1 ')
+
+    # should replace more than one space with just a single one
+    self.gsub!(/[ ]+/,' ')
+
+    # remove spaces, tabs, and new lines from the beginning and enf of file
+    self.strip!
 
     self
   end
--- spec/virastar_spec.rb
+++ spec/virastar_spec.rb
@@ -1,9 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Virastar do
+
   it "should add persian_cleanup method to String class" do
     test = "test string"
-    test.methods.should include("persian_cleanup")
+    test.should respond_to(:persian_cleanup)
   end
 
   it "should replace Arabic kaf with its Persian equivalent" do
@@ -15,7 +16,7 @@ describe Virastar do
     test2.persian_cleanup.should == result2
   end
 
-  it "should replace Arabic Yah with its Persian equivalent" do
+  it "should replace Arabic Yeh with its Persian equivalent" do
     test = "ي"
     test2 = "بيني"
     result = "ی"
@@ -24,7 +25,7 @@ describe Virastar do
     test2.persian_cleanup.should == result2
   end
 
-  it "should replace Arabic numbers (4,5,6) with their Persian equivalent" do
+  it "should replace Arabic numbers with their Persian equivalent" do
     test   = "٠١٢٣٤٥٦٧٨٩"
     result = "۰۱۲۳۴۵۶۷۸۹"
     test.persian_cleanup.should == result
@@ -38,61 +39,107 @@ describe Virastar do
 
   it "should replace English comma and semicolon to their Persian equivalent" do
     test   = ";,"
-    result = "؛ ، "
+    result = "؛ ،"
     test.persian_cleanup.should == result
   end
 
   it "should correct :;,.?! spacing (one space after and no space before)" do
     test   = "گفت : سلام"
     result = "گفت: سلام"
-    puts Diffy::Diff.new(test, result).to_s(:color)
+    #puts Diffy::Diff.new(test, result).to_s(:color) # TODO: char diff
     test.persian_cleanup.should == result
   end
 
-  # it "should replace English qoutes with their Persian equivalent" do
-  #   test  = "''تست''"
-  #   test2 = "'تست'"
-  #   test3 = "\"گفت: سلام\""
-  #   test4 = "``تست''"
-  #   test5 = "`تست`"
-  #   test6 = "``تست``"
-  #   result = result2 = result4 = result5 = result6 = "«تست»"
-  #   result3 = "«گفت: سلام»"
-  #   test.persian_cleanup.should  == result
-  #   test2.persian_cleanup.should == result2
-  #   test3.persian_cleanup.should == result3
-  #   test4.persian_cleanup.should == result4
-  #   test5.persian_cleanup.should == result5
-  #   test6.persian_cleanup.should == result6
-  # end
-  # 
-  # it "should replace three dots with ellipsis" do
-  #   test    = "..."
-  #   result  = "…"
-  #   test2   = "...."
-  #   result2 = "...."
-  #   test3   = "خداحافظ ... به به"
-  #   result3 = "خداحافظ… به به"
-  #   test4   = "........."
-  #   result4 = "........."
+  it "should replace English qoutes with their Persian equivalent" do
+    test  = "''تست''"
+    test2 = "'تست'"
+    test3 = "\"گفت: سلام\""
+    test4 = "`تست`"
+    test5 = "``تست``"
+    result = result2 = result4 = result5 = "«تست»"
+    result3 = "«گفت: سلام»"
+    test.persian_cleanup.should  == result
+    test2.persian_cleanup.should == result2
+    test3.persian_cleanup.should == result3
+    test4.persian_cleanup.should == result4
+    test5.persian_cleanup.should == result5
+  end
+
+  it "should replace three dots with ellipsis" do
+    test    = "..."
+    result  = "…"
+    test2   = "...."
+    result2 = "…"
+    test3   = "خداحافظ ... به به"
+    result3 = "خداحافظ… به به"
+    test4   = "........."
+    result4 = "…"
+    test.persian_cleanup.should   == result
+    test2.persian_cleanup.should  == result2
+    test3.persian_cleanup.should  == result3
+    test4.persian_cleanup.should  == result4
+  end
+
+  it "should convert ه ی to هٔ"  do
+    test = "خانه ی ما"
+    test2 = "خانه ی ما"
+    result = result2 = "خانهٔ ما"
+    test.persian_cleanup.should   == result
+    test2.persian_cleanup.should  == result2
+  end
+
+  it "should replace double dash to ndash and triple dash to mdash" do
+    test    = "--"
+    test2   = "---"
+    result  = "–"
+    result2 = "—"
+    test.persian_cleanup.should   == result
+    test2.persian_cleanup.should  == result2
+  end
+
+  it "should replace more than one space with just a single one" do
+    test   = "  hello   world! \nI'm virastar   "
+    result = "hello world! \nI'm virastar"
+    test.persian_cleanup.should   == result
+  end
+  
+  # it "should fix spacing for () [] {}  “” «» `` '' \"\"" do
+  #   #[["(",")"],["[","]"],["{","}"], ]
+  #   test  = "this is( a test)"
+  #   test2 = "this is ( a test  )"
+  #   test3 = "this is  (  a test )  yeah!"
+  #   test4 = "this is   (a test )  yeah!"
+  #   result  = "this is (a test) "
+  #   result2 = "this is (a test) "
+  #   result3 = "this is (a test) yeah!"
+  #   result4 = "this is (a test) yeah!"
   #   test.persian_cleanup.should   == result
   #   test2.persian_cleanup.should  == result2
   #   test3.persian_cleanup.should  == result3
   #   test4.persian_cleanup.should  == result4
   # end
-  # 
-  # it "should convert ه ی to هٔ" do
-  #   test = "خانه ی ما"
-  #   test2 = "خانه ی ما"
-  #   result = result2 = "خانهٔ ما"
-  #   test.persian_cleanup.should   == result
-  #   test2.persian_cleanup.should  == result2
-  # end
-  # 
-  # it "should replace double dash to ndash and triple dash to mdash"
-  # 
+
   # it "should put zwnj between word and prefix/suffix (ha haye tar tarin mi)"
-  # 
-  # it "should handle specific word آنها"
+  # it "should replace percent and fraction sign to persian ٪٫"
+  # it "should remove unnecessary zwnj char that are succeeded/preceded by a space"
+  # it "should not replace english numbers and ,; in English phrases"
+  #
+  context "aggressive editing" do
+    it "should replace more than one ! or ? mark with just one" do
+      test    = "salam!!!"
+      result  = "salam!"
+      test2   = "چطور؟؟؟"
+      result2 = "چطور؟"
+      test.persian_cleanup.should == result
+      test2.persian_cleanup.should == result2
+    end
+
+    it "should remove all kashida" do
+      test   = "سلامـــت"
+      result = "سلامت"
+      test.persian_cleanup.should == result
+    end
+
+  end
 
 end
